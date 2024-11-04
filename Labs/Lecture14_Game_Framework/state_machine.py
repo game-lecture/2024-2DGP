d1656090229e5b969c2b1c9b4ef5a19813bfe3e4
daehyunlee@zbook <dustinlee@tukorea.ac.kr>
2024-11-05 07:44:16 +0900

    'sync'

---
+++ Labs/Lecture14_Game_Framework/state_machine.py
@@ -0,0 +1,71 @@
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+
+
+
+def start_event(e):
+    return e[0] == 'START'
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+
+
+
+
+class StateMachine:
+    def __init__(self, o):
+        self.o = o
+        self.event_que = []
+
+    def start(self, state):
+        self.cur_state = state
+
+        print(f'Enter into {state}')
+        self.cur_state.enter(self.o, ('START', 0))
+
+    def add_event(self, e):
+        # print(f'    DEBUG: New event {e} added to event Que')
+        self.event_que.append(e)
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
+
+    def update(self):
+        self.cur_state.do(self.o)
+        if self.event_que:
+            event = self.event_que.pop(0)
+            self.handle_event(event)
+
+    def draw(self):
+        self.cur_state.draw(self.o)
+
+    def handle_event(self, e):
+        for event, next_state in self.transitions[self.cur_state].items():
+            if event(e):
+                print(f'Exit from {self.cur_state}')
+                self.cur_state.exit(self.o, e)
+                self.cur_state = next_state
+                print(f'Enter into {self.cur_state}')
+                self.cur_state.enter(self.o, e)
+                return
+
+        # print(f'        Warning: Event [{e}] at State [{self.cur_state}] not handled')
